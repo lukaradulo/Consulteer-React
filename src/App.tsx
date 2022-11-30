da0f3a7d67096fd4f17a0a3a4b95186da6a983e9
lukaradulo <radultec@gmail.com>
2022-11-30 11:46:42 +0100

    responsive mobile home page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <Router>
-      <div className='mx-14'>
+      <div>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
